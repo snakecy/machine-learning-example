374a7c86c2e33c768b4122792c5cdbae974b4555
snakecy <andy21.chow@gmail.com>
2016-01-08 01:28:11 +0800

    add sh file

--- SparkKafkaConsumer/run-demo-spark.sh
+++ SparkKafkaConsumer/run-demo-spark.sh
@@ -1,2 +1,2 @@
 # Predictor
- /home/*/spark/bin/spark-submit --class "KafkaConsumer" --executor-memory 12G --total-executor-cores 32 --master spark://example:7077 /home/*/consumer-kafka/target/scala-2.10/ConsumerApplication-assembly-1.0.jar example-kafka.cloudapp.net:8081,example-kafka.cloudapp.net:9093 hdfs://masters/kafka_consumer_data/rtb_log
+ ./bin/spark-submit --class "KafkaConsumer" --executor-memory 12G --total-executor-cores 32 --master spark://example:7077 ./target/scala-2.10/*-assembly-1.0.jar example-kafka.cloudapp.net:8081,example-kafka.cloudapp.net:8082 hdfs://masters/log
